7e5b06eec046e05fd99db1559502635105d2b1c2
Alex De Pasquale <alexdepasquale1997@gmail.com>
2025-07-03 18:44:36 +0200

    Add max_duplicazioni and invertiEstremi functions to ex.h: Declared max_duplicazioni to retrieve the most frequent item in the list, and invertiEstremi to swap the first and last elements. Updated documentation for both functions to clarify their behavior.

--- Exam_List/src/ex.h
+++ Exam_List/src/ex.h
@@ -1,7 +1,29 @@
 #include "list.h"
 
+/*
+ * Restituisce l'elemento che compare più volte nella lista 'l'.
+ *
+ * Comportamento:
+ *   - Se la lista è vuota, restituisce NULLITEM.
+ *   - Altrimenti, restituisce l'elemento che compare più volte nella lista.
+ */
 item max_duplicazioni(list l);
 
+/*
+ * Restituisce l'elemento centrale della lista 'l' e lo rimuove dalla lista.
+ *
+ * Comportamento:
+ *   - Se la lista è vuota, restituisce NULLITEM.
+ *   - Altrimenti, restituisce l'elemento in posizione floor(n/2), dove n è la lunghezza della lista.
+ *     Questo elemento viene rimosso dalla lista.
+ */
 item itemCentrale(list l);
 
+/*
+ * Inverti gli estremi della lista 'l'.
+ *
+ * Comportamento:
+ *   - Se la lista è vuota o ha un solo elemento, non fa nulla.
+ *   - Altrimenti, inverte il primo e l'ultimo elemento della lista.
+ */
 int invertiEstremi(list l);
